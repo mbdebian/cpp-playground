e3181dd33fd7dfba204d5e3618a9f0dcb60b38a3
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-18 10:22:46 +0100

    working with command line arguments

---
+++ first_steps/cmd_args.cpp
@@ -0,0 +1,8 @@
+// Dealing with command line arguments
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Total arguments: " << argc << std::endl;
+
+    std::cout << "Argument 0: " << argv[0] << std::endl;
+}
