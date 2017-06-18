f93a7b686d19eaa23a056d143abf00fafe2878af
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-18 10:26:05 +0100

    printing command line arguments

--- first_steps/cmd_args.cpp
+++ first_steps/cmd_args.cpp
@@ -3,6 +3,9 @@
 
 int main(int argc, char* argv[]) {
     std::cout << "Total arguments: " << argc << std::endl;
-
-    std::cout << "Argument 0: " << argv[0] << std::endl;
+    std::cout << "Program name: " << argv[0] << std::endl;
+    std::cout << "Program arguments:" << std::endl;
+    for (size_t i = 0; i < argc; i++) {
+        std::cout << "\t" << i << " ---> " << argv[i] << std::endl;
+    }
 }
