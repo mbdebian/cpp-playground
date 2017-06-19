aa746b2e919cf7adfb7efe40439c166b4b283e58
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-19 01:28:59 +0100

    boolean data type

--- first_steps/primitive_types.cpp
+++ first_steps/primitive_types.cpp
@@ -5,6 +5,7 @@ int main(int argc, char* argv[]) {
     float radix = 2.37;
     double pi = 3.14159;
     long double pval = 0.78430092337512227;
+    bool condition = false;
 
     int ip = pi;
     std::cout << "double " << pi << " has been truncated to int " << ip << std::endl;
