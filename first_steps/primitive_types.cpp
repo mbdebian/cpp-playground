7f61e2c31de263daf7b1e232f30763bb38638996
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-19 14:06:07 +0100

    example of uninitialized variable

--- first_steps/primitive_types.cpp
+++ first_steps/primitive_types.cpp
@@ -6,7 +6,11 @@ int main(int argc, char* argv[]) {
     double pi = 3.14159;
     long double pval = 0.78430092337512227;
     bool condition = false;
+    int i;      // declared but not initialized
 
     int ip = pi;
     std::cout << "double " << pi << " has been truncated to int " << ip << std::endl;
+    std::cout << "uninitialized i = "
+                << i
+                << std::endl;
 }
