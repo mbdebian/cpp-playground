26d9b43e884c5cad50da947b00240eae39575444
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-19 01:25:44 +0100

    first steps dealing with primitive types

---
+++ first_steps/primitive_types.cpp
@@ -0,0 +1,8 @@
+// Quick example dealing with primitive types
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    float radix = 2.37;
+    double pi = 3.14159;
+    long double pval = 0.78430092337512227;
+}
