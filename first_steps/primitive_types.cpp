20eb53ae3717f90646aea2943cf90dc85b16677c
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-20 07:41:21 +0100

    const from non-const variable

--- first_steps/primitive_types.cpp
+++ first_steps/primitive_types.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
     // constexpr - value is known at compile time and does not change for the life of the application
     // const - a runtime value that once assigned in the application should not change
     const int you_cant_change_me = 12;
-    int const you_cant_change_me_either = 20;
+    int const you_cant_change_me_either = pi;
 
     int ip = pi;
     std::cout << "double " << pi << " has been truncated to int " << ip << std::endl;
