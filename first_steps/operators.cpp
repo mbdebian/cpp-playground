4bf0c569f86b676070be1b2de6a35b2475ea1e90
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-20 08:23:24 +0100

    example of precedence + /

--- first_steps/operators.cpp
+++ first_steps/operators.cpp
@@ -9,8 +9,13 @@ int main(int argc, char* argv[]) {
     double a = 3.25;
     double b = 4.75;
     double c = 1.25;
+    int int_a = 3;
+    int int_b = 7;
+    int int_c = 2;
 
     // The following will not compile because the expression returning type is double
     // PRINT_INT_EXPR(a + b + c);
     PRINT_DOUBLE_EXPR(a + b + c);
+    PRINT_DOUBLE_EXPR(a + b - c);
+    PRINT_INT_EXPR(int_a + int_b / int_c);
 }
