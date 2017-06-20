13f25b1acc19b4e77d551b2bf9f522707a87f65f
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-20 07:40:50 +0100

    constexpr vs const

--- first_steps/primitive_types.cpp
+++ first_steps/primitive_types.cpp
@@ -7,6 +7,8 @@ int main(int argc, char* argv[]) {
     long double pval = 0.78430092337512227;
     bool condition = false;
     int i;      // declared but not initialized
+    // constexpr - value is known at compile time and does not change for the life of the application
+    // const - a runtime value that once assigned in the application should not change
     const int you_cant_change_me = 12;
     int const you_cant_change_me_either = 20;
 
