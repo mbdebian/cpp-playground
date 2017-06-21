c34da2053e1ef180a6f23ddad1cb16fbfcb89271
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-21 10:16:59 +0100

    playing with statements

---
+++ first_steps/playing_with_statements.cpp
@@ -0,0 +1,10 @@
+// This is a simple example on statements
+#include <iostream>
+#include <string>
+
+int main(int arc, char* argv[]) {
+    std::string name = "";
+    std::cout << "Please, enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello " << name << "!" << std::endl;
+}
