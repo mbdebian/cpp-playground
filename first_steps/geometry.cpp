fbf91421576fdd425d25384176f7c6fefd404528
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-23 06:03:46 +0100

    implementation for the declarations of geometry.h

---
+++ first_steps/geometry.cpp
@@ -0,0 +1,7 @@
+// geometry.cpp file that contains the definitions for geometry.h
+#include <cmath>
+#include "geometry.h"
+
+double calc_area_of_circle(double radius) {
+    return 3.1415926535 * std::pow(radius, 2);
+}
