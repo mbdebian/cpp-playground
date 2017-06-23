7583bab93f4dd6746d5d192ca06c6f40095d85ab
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-23 22:47:45 +0100

    print_usage

--- first_steps/calculator/main.cpp
+++ first_steps/calculator/main.cpp
@@ -3,7 +3,11 @@
     returns the operation on the two numbers
 
 */
+#include <iostream>
+
+void print_usage(char* prog_name) {
+    std::cout << prog_name << " <num 1> operation <num 2>" << std::endl;
+}
 
 int main(int argc, char* argv[]) {
-    
 }
