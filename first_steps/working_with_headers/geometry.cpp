b66c3cabbad74d031e44934140cf4acd69e784b4
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-23 06:13:48 +0100

    main method removed

--- first_steps/working_with_headers/geometry.cpp
+++ first_steps/working_with_headers/geometry.cpp
@@ -6,6 +6,4 @@ double calc_area_of_circle(double radius) {
     return 3.1415926535 * std::pow(radius, 2);
 }
 
-// just to get the code to compile, but when this file is used as a library, there should be no "main" method
-int main() {
-}
+// This is just a library, it doesn't need a main method
