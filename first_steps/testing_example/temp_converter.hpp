c222c557e5c1323c6b82bf3181185562ac78db07
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-24 09:07:15 +0100

    testing example, test converter full definition header

---
+++ first_steps/testing_example/temp_converter.hpp
@@ -0,0 +1,10 @@
+// Header file that contains complete definitions
+#ifndef TEMP_CONVERTER_HPP
+#define TEMP_CONVERTER_HPP
+
+inline
+double celsius_to_fahrenheit(double celsius_temp) {
+    return (celsius_temp * 9 / 5) + 32;
+}
+
+#endif // TEMP_CONVERTER_HPP
