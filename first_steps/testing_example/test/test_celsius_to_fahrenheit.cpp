3c5dbad5edcde281e5f6f0a3877317be12cd3332
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-25 08:21:55 +0100

    first version of our new tests

--- first_steps/testing_example/test/test_celsius_to_fahrenheit.cpp
+++ first_steps/testing_example/test/test_celsius_to_fahrenheit.cpp
@@ -1,3 +1,11 @@
 // Test file
 #include <libunittest/all.hpp>
 #include "temp_converter.hpp"
+
+//using namespace unittest::assertions;
+
+// This is a 'macro' that creates a test
+// SPOT is a keyword in libunittest, that, when the test fails, refer to SPOT to point us where the test fails
+TEST(test_celsius_to_fahrenheit) {
+    unittest::assertions::assert_true(celsius_to_fahrenheit(15) == 59.0, SPOT);
+}
