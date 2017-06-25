33b73ceba80d2888482e954ec78e3ab4a635b34a
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-25 08:32:07 +0100

    implementation of temperature conversion

--- first_steps/testing_example/src/temp_converter.hpp
+++ first_steps/testing_example/src/temp_converter.hpp
@@ -1,1 +1,10 @@
 // This is the file implementing the temperature conversion from Celsius to Fahrenheit
+#ifndef TEMP_CONVERTER_HPP
+#define TEMP_CONVERTER_HPP
+
+inline
+double celsius_to_fahrenheit(double celsius_temp) {
+    return (c * 9/5) + 32;
+}
+
+#endif // TEMP_CONVERTER_HPP
