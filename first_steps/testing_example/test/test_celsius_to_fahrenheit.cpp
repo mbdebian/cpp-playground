0aaab526d9333a8d68635258d3bc65ebbb214146
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-25 08:44:13 +0100

    multiple unit tests

--- first_steps/testing_example/test/test_celsius_to_fahrenheit.cpp
+++ first_steps/testing_example/test/test_celsius_to_fahrenheit.cpp
@@ -8,7 +8,10 @@
 
 // This is a 'macro' that creates a test
 // SPOT is a keyword in libunittest, that, when the test fails, refer to SPOT to point us where the test fails
-TEST(test_celsius_to_fahrenheit) {
+TEST(test_celsius_to_fahrenheit_for_15) {
     unittest::assertions::assert_true(celsius_to_fahrenheit(15) == 59.0, SPOT);
+}
+
+TEST(test_celsius_to_fahrenheit_for_19) {
     unittest::assertions::assert_true(celsius_to_fahrenheit(19) == 66.2, SPOT);
 }
