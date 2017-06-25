132669f43ca3f5f3a6785c8d89d2ac8e936ea119
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-25 07:58:54 +0100

    assertions in the test

--- first_steps/testing_example/test.cpp
+++ first_steps/testing_example/test.cpp
@@ -3,5 +3,7 @@
 using c2f = celsius_to_fahrenheit;
 
 void test_celsius_to_fahrenheit(double celsius) {
-
+    ASSERT_TRUE(c2f(15) == 59.0);
+    ASSERT_TRUE(c2f(19) == 66.2);
+    ASSERT_TRUE(c2f(23) == 73.4);
 }
