b284858b7d20296fc458d269cd952be60ebe15ca
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-25 08:01:10 +0100

    documenting comments

--- first_steps/testing_example/test.cpp
+++ first_steps/testing_example/test.cpp
@@ -3,6 +3,7 @@
 using c2f = celsius_to_fahrenheit;
 
 void test_celsius_to_fahrenheit(double celsius) {
+    // TDD rule: only 1 test per method, but we run a few of them for brevity purposes
     ASSERT_TRUE(c2f(15) == 59.0);
     ASSERT_TRUE(c2f(19) == 66.2);
     ASSERT_TRUE(c2f(23) == 73.4);
