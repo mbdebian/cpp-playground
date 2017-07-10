bff239cacbea77d8239b41e53156a55dc2950581
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-10 14:28:03 +0100

    test suite and first test placeholder

--- first_steps/temp_conversion_unittest++/tests/celsius_to_fahrenheit_test.hpp
+++ first_steps/temp_conversion_unittest++/tests/celsius_to_fahrenheit_test.hpp
@@ -1,6 +1,12 @@
 #ifndef CELSIUS_TO_FAHRENHEIT_TEST_HPP
 #define CELSIUS_TO_FAHRENHEIT_TEST_HPP
 
+#include <UnitTest++.h>
 
+SUITE(TemperatureConversionTests) {
+    TEST(test_celsius_to_fahrenheit) {
+        
+    }
+}
 
 #endif // CELSIUS_TO_FAHRENHEIT_TEST_HPP
