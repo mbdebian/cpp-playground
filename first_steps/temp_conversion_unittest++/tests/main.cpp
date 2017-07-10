90264b2add9f5ff004b144ed7f2ff56b86d688da
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-10 14:31:13 +0100

    call to run all tests that have been registered

--- first_steps/temp_conversion_unittest++/tests/main.cpp
+++ first_steps/temp_conversion_unittest++/tests/main.cpp
@@ -1,1 +1,7 @@
 // This will be the unittest driver
+#include <UnitTest++.h>
+
+int main(int argc, char* argv[]) {
+    // We call this method to call all the unit tests that have been registered
+    return UnitTest::RunAllTests();
+}
