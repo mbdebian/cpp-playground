a59563945505be0f0ce5e91b195d56323f4c30e9
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-10 14:32:07 +0100

    include the tests to be registered

--- first_steps/temp_conversion_unittest++/tests/main.cpp
+++ first_steps/temp_conversion_unittest++/tests/main.cpp
@@ -1,5 +1,6 @@
 // This will be the unittest driver
 #include <UnitTest++.h>
+#include "celsius_to_fahrenheit_test.hpp"
 
 int main(int argc, char* argv[]) {
     // We call this method to call all the unit tests that have been registered
