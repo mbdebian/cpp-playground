a22e4b5e1dd0bcfea068bcaab283ddb534ec7eb4
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-10 14:38:33 +0100

    temp conversion inline implementation

--- first_steps/temp_conversion_unittest++/src/temp_conversion.hpp
+++ first_steps/temp_conversion_unittest++/src/temp_conversion.hpp
@@ -1,1 +1,10 @@
 // Implements the temperature conversion function
+#ifndef TEMP_CONVERSION_HPP
+#define TEMP_CONVERSION_HPP
+
+inline
+double celsius_to_fahrenheit(double c) {
+    return (c * 9/5) + 32;
+}
+
+#endif // TEMP_CONVERSION_HPP
